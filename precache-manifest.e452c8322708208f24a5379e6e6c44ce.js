cde6dc085cc48e6873c9a7bfb2fbba738951b26e
cityji <saurabh.paytmservices@gmail.com>
2022-07-07 07:15:28 +0530

    Updates

---
+++ precache-manifest.e452c8322708208f24a5379e6e6c44ce.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "46646705405628cb7c00",
+    "url": "/static/css/main.bdcccced.chunk.css"
+  },
+  {
+    "revision": "46646705405628cb7c00",
+    "url": "/static/js/main.46646705.chunk.js"
+  },
+  {
+    "revision": "2e98f54001ba0032e53b",
+    "url": "/static/js/1.2e98f540.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "39dc1746dbb82da261d0f020784dca7b",
+    "url": "/static/media/student.39dc1746.jpg"
+  },
+  {
+    "revision": "10ba4503466db273e140984bcefb1ceb",
+    "url": "/static/media/cv.10ba4503.pdf"
+  },
+  {
+    "revision": "993e408218a639933b0e11bee720e1ba",
+    "url": "/index.html"
+  }
+];
